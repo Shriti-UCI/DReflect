88d3381c7ef8bf95af90dece65d816028d578828
neerajkumar <neerajpkumar@gmail.com>
2016-07-12 22:43:59 -0400

    Adding Action, ActionEvent and StateChangeEvent classes.

---
+++ minukucore/src/main/java/edu/umich/si/inteco/minukucore/action/Action.java
@@ -0,0 +1,11 @@
+package edu.umich.si.inteco.minukucore.action;
+
+/**
+ * Action {@link edu.umich.si.inteco.minukucore.event.Subscribe subscribes} to an event
+ * {@link edu.umich.si.inteco.minukucore.event.ActionEvent} and handles it.
+ * This decouples an Action from other parts of the system.
+ *
+ * Created by neerajkumar on 7/12/16.
+ */
+public interface Action {
+}
---
+++ minukucore/src/main/java/edu/umich/si/inteco/minukucore/event/StateChangeEvent.java
@@ -0,0 +1,7 @@
+package edu.umich.si.inteco.minukucore.event;
+
+/**
+ * Created by neerajkumar on 7/12/16.
+ */
+public class StateChangeEvent {
+}
