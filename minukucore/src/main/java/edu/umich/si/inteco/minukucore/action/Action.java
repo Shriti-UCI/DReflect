ab11df9567aa1bf215b74a5008d2c159f375858e
neerajkumar <neerajpkumar@gmail.com>
2016-07-12 22:59:01 -0400

    Adding handleAction method to Action class.

--- minukucore/src/main/java/edu/umich/si/inteco/minukucore/action/Action.java
+++ minukucore/src/main/java/edu/umich/si/inteco/minukucore/action/Action.java
@@ -1,5 +1,8 @@
 package edu.umich.si.inteco.minukucore.action;
 
+import edu.umich.si.inteco.minukucore.event.ActionEvent;
+import edu.umich.si.inteco.minukucore.event.Subscribe;
+
 /**
  * Action {@link edu.umich.si.inteco.minukucore.event.Subscribe subscribes} to an event
  * {@link edu.umich.si.inteco.minukucore.event.ActionEvent} and handles it.
@@ -8,4 +11,8 @@ package edu.umich.si.inteco.minukucore.action;
  * Created by neerajkumar on 7/12/16.
  */
 public interface Action {
+
+    @Subscribe
+    public <T extends ActionEvent> void handleEvent(T actionEvent);
+
 }
