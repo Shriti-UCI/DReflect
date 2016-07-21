8951caebc22a247f46bcae31eae7a851805e9b5e
neerajkumar <neerajpkumar@gmail.com>
2016-07-21 15:45:15 -0400

    1. Changing dependsOn signature for Stream

--- minukucore/src/main/java/edu/umich/si/inteco/minukucore/stream/Stream.java
+++ minukucore/src/main/java/edu/umich/si/inteco/minukucore/stream/Stream.java
@@ -42,7 +42,7 @@ public interface Stream<T extends DataRecord> extends Queue<T> {
      *
      * @return the list of DataRecord types
      */
-    public<T extends DataRecord> List<Class<T>> dependsOnDataRecord();
+    public List<Class<? extends DataRecord>> dependsOnDataRecord();
 
     /**
      * Get the type of stream - from_device, from_user, from_question
