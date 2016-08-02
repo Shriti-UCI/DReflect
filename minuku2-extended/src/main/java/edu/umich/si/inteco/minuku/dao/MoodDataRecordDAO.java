c0a30d0703d3b5729ef08b891ccf6ea490825c94
Neeraj Kumar <neerajpkumar@gmail.com>
2016-08-02 15:01:44 -0400

    For any DAO, the getLastN method returns a future. The data is stored in firebase with the following structure:
    -root
    --mood
    ---user
    ----date1
    -----record1
    -----record2
    ----date2
    -----record1
    -----record2
    -----record3
    
    To get the last X records of a type, the DAO class needs to iterate over each date (Starting from today, backwards) and attempt to keep fetching this data one by one until N records are fetched.
    
    Now, the fetching of this data via Firebase API is in an asychronous fashion, which makes it very important to synchronize this process of looping. Hence using the following algorithm, the DAO class implements the getLastN functionality.
    
    Future getLastN(int someN) {
         call attemptToGetLastNValues(someN, userEmail, today, colletionOfResults, settableFutureObject);
    }
    
    void attemptToGetLastNValues(aNumber, aUserEmailString, aDate, aList, aFuture) {
         newDate = aDate - 1day;
         asyncrhonously try to get values from firebase for newDate
                     ----> onFirebaseDataResult (result) {
                               // convert results to list call is listOfResultsForToday
                               // if listOfResultsForToday > aNumer, it means that we have the total number of results required
                                    // aFuture.set()
                               // else
                                    // call attemptToGetLastNValues(newDate);
                     }
                     ----> onFirebaseDateFailure {
                              // means that the no records exist for aDate in firebase, which means we have exhausted all our entries in firebase.
                              // Set the future and return without continout the chain of calls.
                     }
    }

--- minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/dao/MoodDataRecordDAO.java
+++ minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/dao/MoodDataRecordDAO.java
@@ -6,9 +6,12 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.SettableFuture;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,10 +21,12 @@ import java.util.concurrent.Future;
 
 import edu.umich.si.inteco.minuku.config.Constants;
 import edu.umich.si.inteco.minuku.config.UserPreferences;
+import edu.umich.si.inteco.minuku.manager.MinukuStreamManager;
 import edu.umich.si.inteco.minuku.model.ImageDataRecord;
 import edu.umich.si.inteco.minuku.model.MoodDataRecord;
 import edu.umich.si.inteco.minukucore.dao.DAO;
 import edu.umich.si.inteco.minukucore.dao.DAOException;
+import edu.umich.si.inteco.minukucore.stream.Stream;
 import edu.umich.si.inteco.minukucore.user.User;
 
 /**
@@ -78,12 +83,65 @@ public class MoodDataRecordDAO implements DAO<MoodDataRecord> {
         return settableFuture;    }
 
     @Override
-    public Future<List<MoodDataRecord>> getLast(int N) throws DAOException {
-        return null;
+    public Future<List<MoodDataRecord>> getLast(final int N) throws DAOException {
+        final SettableFuture<List<MoodDataRecord>> settableFuture = SettableFuture.create();
+        final Date today = new Date();
+
+        final List<MoodDataRecord> lastNRecords = Collections.synchronizedList(
+                new ArrayList<MoodDataRecord>());
+
+        MoodDataRecordDAO.getLastNValues(N, myUser.getEmail(), today, lastNRecords, settableFuture);
+
+        return settableFuture;
     }
 
     @Override
     public void update(MoodDataRecord oldEntity, MoodDataRecord newEntity) throws DAOException {
 
+    }
+
+    private static final void getLastNValues(final int N,
+                                             final String userEmail,
+                                             final Date someDate,
+                                             final List<MoodDataRecord> synchronizedListOfRecords,
+                                             final SettableFuture settableFuture) {
+        Firebase firebaseRef = new Firebase(Constants.FIREBASE_URL_MOODS)
+                .child(userEmail)
+                .child(new SimpleDateFormat("MMddyyyy").format(someDate).toString());
+
+        firebaseRef.limitToLast(N).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get all the values in firebase list as map
+                Map<String, MoodDataRecord> moodListMap =
+                        (HashMap<String,MoodDataRecord>) dataSnapshot.getValue();
+
+                // Add values in the map as a list to the list of results.
+                synchronizedListOfRecords.addAll((List)moodListMap.values());
+
+                // if after adding records for "Today" (moving day for each loop),
+                // the number of records in list exceed N, set the future.
+                if(synchronizedListOfRecords.size() > N) {
+                    settableFuture.set(Lists.reverse(synchronizedListOfRecords));
+                } else {
+                    int newN = N - moodListMap.values().size();
+                    Date newDate = new Date(someDate.getTime() - 26 * 60 * 60 * 1000); /* -1 Day */
+                    MoodDataRecordDAO.getLastNValues(newN,
+                            userEmail,
+                            newDate,
+                            synchronizedListOfRecords,
+                            settableFuture);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+                // This would mean that the firebase ref does not exist thereby meaning that
+                // the number of entries for all dates are over before we could get the last N
+                // results
+                settableFuture.set(synchronizedListOfRecords);
+            }
+        });
     }
 }
