928296fa4f6c451af7d51952c8496fb267f8a0f9
neerajkumar <neerajpkumar@gmail.com>
2016-08-17 00:04:05 -0400

    Some code duplication existed between FreeResponse and MCQ DAOs. This would also have continued for LikertScale questions. Moving common code into AbstractQuestionsDAO class, and making FreeResponse and MCQ extend it.

--- minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/config/Constants.java
+++ minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/config/Constants.java
@@ -18,7 +18,8 @@ public class Constants {
     public static final String FIREBASE_URL_LOCATION = FIREBASE_URL + "/location";
     public static final String FIREBASE_URL_SEMANTIC_LOCATION = FIREBASE_URL + "/semantic_location";
     public static final String FIREBASE_URL_QUESTIONS = FIREBASE_URL + "/questions";
-
+    public static final String FIREBASE_URL_MCQ = FIREBASE_URL_QUESTIONS + "/mcq";
+    public static final String FIREBASE_URL_FREE_RESPONSE = FIREBASE_URL_QUESTIONS + "/freeresponse";
 
 
 
